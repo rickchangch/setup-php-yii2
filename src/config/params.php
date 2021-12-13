ba51a04b3f6d31b07031db0ee40746d49f1d463b
Rick Chang <rick.chang@staff.nueip.com>
2021-12-13 18:05:02 +0800

    feature: init yii2-basic framwork

---
+++ src/config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+
+    // Cookie
+    'cookieValidationKey' => '1956f3c2-a2d4-4bd0-9001-f4d4fcb9460c',
+];
